fe5a4a060515e451a213bc257ff25bc0c7e31b69
tandongs <tandongsheng@jollycorp.com>
2018-01-12 00:18:36 +0800

    update

--- index.js
+++ index.js
@@ -1,71 +1,17 @@
 require('shelljs/global');
-var AipOcrClient   = require("baidu-aip-sdk").ocr;
-var request        = require('request');
-var cheerio        = require('cheerio');
-var fs             = require('fs');
-var APP_ID         = 10672764;
-var images         = require("images");
-var API_KEY        = "2e4o0q4XCgPZFWPAkxBc2dQi";
-var SECRET_KEY     = "Siz8xYL2MP52IPTd8DPVaNAX7C7TVg8k";
-var client         = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
-const io           = require('socket.io-client');
-
-
-var timestamp=new Date().getTime();
-exec('adb shell /system/bin/screencap -p /sdcard/'+timestamp+'.png && adb pull /sdcard/'+timestamp+'.png D:\\jietu\\images\\'+timestamp+'.png', function(code, stdout, stderr) {
-	var imageBuffer    = new Buffer(fs.readFileSync('images/'+timestamp+'.png'));
-	images(imageBuffer).draw(images("fugai.png"), 0, 0).resize(720, null).save("images\\build\\"+timestamp+".png",{quality: 30});
-	var newImageBase64 = imageBuffer.length > 102400 ? images(imageBuffer).draw(images("fugai.png"), 0, 0).encode("png", { operation: 70 }).toString("base64") : imageBuffer.toString("base64");
-
-	console.log('imageBuffer：'+imageBuffer.length, '---newImageBase64：'+newImageBase64.length);
-
-	//// 调用通用文字识别, 图片参数为本地图片
-	client.generalBasic(newImageBase64).then(function(result) {
-		var imageTextObj = result.words_result;
-
-		var newKey = '';
-		
-		var answer   = [];
-		var question = [];
-		
-		for(item in imageTextObj){
-			var index = parseInt(item);
-			if(imageTextObj[index].words === '7777'){
-				question = imageTextObj.slice(0,imageTextObj.length - 5);
-				answer   = imageTextObj.slice(imageTextObj.length - 3,imageTextObj.length);
-			}
-		}
-		
-		for(item in question){
-			newKey+=question[item].words;
-		}
-		
-		var obj = {
-			question: newKey,
-			answer: answer
-		}
-		
-//		console.log(obj);
-
-		getSearchContent(obj);
-
-	}).catch(function(err) {
-		// 如果发生网络错误
-		console.log(err);
-		console.log('asdfsfd');
-	});
-
-});
+const AipOcrClient   = require("baidu-aip-sdk").ocr;
+const request        = require('request');
+const cheerio        = require('cheerio');
+const fs             = require('fs');
+const images         = require("images");
+const timestamp      = new Date().getTime();
+const socket         = require('socket.io-client').connect('http://localhost:3538');
 
 // 把识别的关键字当key请求百度返回搜索结果
 const getSearchContent = (obj) => {
-	var socket = io.connect('http://localhost:3538');
-	socket.on('connect', function (data) {
-		socket.emit('sendKeyToServer', obj);
-	});
 	
-	request('http://www.baidu.com/s?wd='+encodeURI(obj.question), function (error, response, body) {
-		if (!error && response.statusCode == 200) {
+	request('http://www.baidu.com/s?wd='+encodeURI(obj.question), (error, response, body) => {
+		if (!error && response.statusCode === 200) {
 			var $ = cheerio.load(body);
 			var str = '';
 			for(item in $('.result.c-container')){
@@ -74,28 +20,77 @@ const getSearchContent = (obj) => {
 				}
 			}
 			
-			var newArr = [];
+			const newArr = [];
 			
 			for(item in obj.answer){
-				var value = obj.answer[item].words;
-				var arr = str.split(value);
+				const value = obj.answer[item].words;
+				const arr = str.split(value);
 				newArr.push({item: value, count: arr.length - 1});
 			}
-			console.log('----------------------------------------------------------------------------------------');
 			
-			
-			var newOBj = {
+			const newOBj = {
 				title: obj.question,
 				data: newArr
 			}
 			
-			console.log(newOBj);
-			
+			socket.emit('sendKeyToServer', obj);
 			socket.emit('sendKeyToServerAnswer', newOBj);
 			
 		}
 	});
 }
+
+// 根据图片识别文字
+const getImageText = () => {
+	const imageBuffer    = new Buffer(fs.readFileSync('images/'+timestamp+'.png'));
+	const newImageBase64 = imageBuffer.length > 102400 ? images(imageBuffer).draw(images("mask.png"), 0, 0).encode("png", { operation: 70 }).toString("base64") : imageBuffer.toString("base64");
+	images(imageBuffer).draw(images("mask.png"), 0, 0).resize(720, null).save("images\\build\\"+timestamp+".png",{quality: 30});
+	
+	console.log('imageBuffer：'+imageBuffer.length, '---newImageBase64：'+newImageBase64.length);
+	
+	const APP_ID         = 10672764;
+	const API_KEY        = "2e4o0q4XCgPZFWPAkxBc2dQi";
+	const SECRET_KEY     = "Siz8xYL2MP52IPTd8DPVaNAX7C7TVg8k";
+	const client         = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY);
+	
+	//// 调用通用文字识别, 图片参数为本地图片
+	client.generalBasic(newImageBase64).then((result) => {
+		const imageTextObj = result.words_result;
+		
+		let newKey   = '';
+		let answer   = [];
+		let question = [];
+		
+		// 从源数据格式化出问题与答案
+		for(item in imageTextObj){
+			const index = parseInt(item);
+			if(imageTextObj[index].words === '7777'){
+				question = imageTextObj.slice(0,imageTextObj.length - 5);
+				answer   = imageTextObj.slice(imageTextObj.length - 3,imageTextObj.length);
+			}
+		}
+		
+		// 生成标题
+		for(item in question){
+			newKey += question[item].words;
+		}
+		
+		var obj = {
+			question: newKey,
+			answer: answer
+		}
+		
+		getSearchContent(obj);
+		
+	}).catch(function(err) {
+		// 如果发生网络错误
+		console.log(err);
+	});
+}
+
+exec('adb shell /system/bin/screencap -p /sdcard/'+timestamp+'.png && adb pull /sdcard/'+timestamp+'.png D:\\jietu\\images\\'+timestamp+'.png', (code, stdout, stderr) => {
+	getImageText();
+});
 
 
 
